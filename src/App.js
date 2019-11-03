ac84c19c0e2b213ce55e538c98c4e144b99acb5a
Marcelo <mail.marcelo.as@gmail.com>
2019-11-03 16:25:31 -0500

    Add image SVG

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Space from './space-kurzgesagt.svg';
 import './App.css';
 
 class MainApp extends React.Component{
   render(){
     return(
       <div>
+        <div className="header">
+          <img alt="Universe" src={Space} className="App-header-img"/>
+        </div>
         <div className="App-title"><h1>Explore Infinity</h1></div>
         <article className="App-description">
           Exploring the infinity of the cosmos
