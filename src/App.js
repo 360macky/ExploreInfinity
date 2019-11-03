1710a88065f9105c3ff8ebd1e11427032032c5b3
Marcelo <mail.marcelo.as@gmail.com>
2019-11-03 15:28:41 -0500

    Yes, is React. Give me a moment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class MainApp extends React.Component{
+  render(){
+    return(
+      <div>
+        <div className="App-title"><h1>Explore Infinity</h1></div>
+        <article className="App-description">
+          Exploring the infinity of the cosmos
+        </article>
+      </div>
+    )
+  }
+}
+
+export default MainApp;
