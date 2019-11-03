8c7ad25bec09bf69d7da61a6f54fda43f8e4b031
Marcelo <mail.marcelo.as@gmail.com>
2019-11-03 17:22:42 -0500

    Add NewsResources

---
+++ src/NewsResources.js
@@ -0,0 +1,17 @@
+const NewsResources = [
+    {
+        id: 1,
+        title: "Astronomy Section - Science Magazine",
+        link: "https://www.sciencemag.org/journal-category/astronomy"
+    },
+    {
+        id: 2,
+        title: "Space News - Space.com",
+        link: "https://www.space.com/news"
+    },,
+    {
+        id: 3,
+        title: "Astronomy News - Sci-News",
+        link: "http://www.sci-news.com/news/astronomy"
+    }
+]
